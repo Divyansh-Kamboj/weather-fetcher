c506b38b45b2c377b63313ebe299a52d0ce721b2
Divyansh Kamboj <divyanshkamboj@Karishmas-MacBook-Air.local>
2022-03-19 21:51:21 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import requests
+
+API_KEY = "171c2015900fa7a92da055495cde2c09"
+BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
+
+city = input("Enter a city name: ")
+request_url = f"{BASE_URL}?appid={API_KEY}&q={city}"
+response = requests.get(request_url)
+
+if response.status_code == 200:
+    data = response.json()
+    weather = data['weather'][0]['description']
+    temperature = round(data["main"]["temp"] - 273.15, 2)
+
+    print("Weather:", weather)
+    print("Temperature:", temperature, "celsius")
+else:
+    print("An error occurred.")
